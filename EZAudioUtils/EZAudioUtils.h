dc431e3b36d5056f81f2a98547def52e2b0bd8bc
peterlee <925460675@qq.com>
2021-05-26 11:08:03 +0800

    setupEAAudioUtils

---
+++ EZAudioUtils/EZAudioUtils.h
@@ -0,0 +1,18 @@
+//
+//  EZAudioUtils.h
+//  EZAudioUtils
+//
+//  Created by peterlee on 2021/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EZAudioUtils.
+FOUNDATION_EXPORT double EZAudioUtilsVersionNumber;
+
+//! Project version string for EZAudioUtils.
+FOUNDATION_EXPORT const unsigned char EZAudioUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZAudioUtils/PublicHeader.h>
+#import <EZAudioUtils/EZAudio.h>
+
